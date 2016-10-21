13a1f603b050091d9ab36b26802436ca582b10da
HDT3213 <hdt3213@outlook.com>
2016-10-21 15:12:23 +0800

    sync

--- RNN/demo.py
+++ RNN/demo.py
@@ -2,36 +2,52 @@ import tensorflow as tf
 from tensorflow.python.ops import rnn, rnn_cell
 import numpy as np
 
-batch_size = 1
-n_steps = 3
-n_input = 2
-n_hidden = 5
-n_classes = 2
-learning_rate = 0.05
-limit = 1000
 
-# Split to get a list of 'n_steps' tensors of shape (batch_size, n_input)
-input_layer = [tf.placeholder("float", [n_steps, n_input])]  # * batch_size
-label_layer = tf.placeholder("float", [n_steps, n_classes])
+class RecurrentNeuralNetwork:
+    def __init__(self):
+        self.session = tf.Session()
+        self.input_layer = None
+        self.label_layer = None
+        self.weights = None
+        self.biases = None
+        self.lstm_cell = None
+        self.prediction = None
+        self.loss = None
+        self.trainer = None
 
-weights = tf.Variable(tf.random_normal([n_hidden, n_classes]))
-biases = tf.Variable(tf.random_normal([n_classes]))
-lstm_cell = rnn_cell.BasicLSTMCell(n_hidden, forget_bias=1.0)
-outputs, states = rnn.rnn(lstm_cell, input_layer, dtype=tf.float32)
-prediction = tf.matmul(outputs[-1], weights) + biases
-loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(prediction, label_layer))
-optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
-initer = tf.initialize_all_variables()
+    def __del__(self):
+        self.session.close()
 
-# Current data input shape: (batch_size, n_steps, n_input)
-train_x = np.array([[1, 1, 1, 1, 1, 1]])
-train_x = train_x.reshape((batch_size, n_steps, n_input))
-train_y = np.array([[1, 1, 1, 1, 1, 1]])
-train_y = train_y.reshape((n_steps, n_input))
-test_x = np.array([[]])
+    def train(self, train_x, train_y, learning_rate=0.05, limit=1000, batch_size=1, step_n=3, input_n=2, hidden_n=5, output_n=2):
+        self.input_layer = [tf.placeholder("float", [step_n, input_n]) for i in range(batch_size)]
+        self.label_layer = tf.placeholder("float", [step_n, output_n])
+        self.weights = tf.Variable(tf.random_normal([hidden_n, output_n]))
+        self.biases = tf.Variable(tf.random_normal([output_n]))
+        self.lstm_cell = rnn_cell.BasicLSTMCell(hidden_n, forget_bias=1.0)
+        outputs, states = rnn.rnn(self.lstm_cell, self.input_layer, dtype=tf.float32)
+        self.prediction = tf.matmul(outputs[-1], self.weights) + self.biases
+        self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(self.prediction, self.label_layer))
+        self.trainer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(self.loss)
+        initer = tf.initialize_all_variables()
+        train_x = train_x.reshape((batch_size, step_n, input_n))
+        train_y = train_y.reshape((step_n, output_n))
+        # run graph
+        self.session.run(initer)
+        for i in range(limit):
+            self.session.run(self.trainer, feed_dict={self.input_layer[0]: train_x[0], self.label_layer: train_y})
 
-with tf.Session() as session:
-    session.run(initer)
-    for i in range(limit):
-        session.run(optimizer, feed_dict={input_layer[0]: train_x[0], label_layer: train_y})
-    print session.run(prediction, feed_dict={input_layer[0]: train_x[0]})
+    def predict(self, test_x):
+        return self.session.run(self.prediction, feed_dict={self.input_layer[0]: test_x})
+
+    def test(self):
+        train_x = np.array([[1, 2, 3, 4, 5, 6]])
+        train_y = np.array([[1, 2, 3, 4, 5, 6]])
+        self.train(train_x, train_y, batch_size=1, step_n=3, input_n=2)
+        test_x = np.array([[1, 1], [2, 2], [3, 3]])
+        # test_x = train_x.reshape((1, 3, 2))[0]
+        print self.predict(test_x)
+
+
+if __name__ == '__main__':
+    nn = RecurrentNeuralNetwork()
+    nn.test()
